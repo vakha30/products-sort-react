3282654be7496cdb2779a3bed585af6453c3e364
mansur <gvardeez95@gmail.com>
2021-03-20 13:35:10 +0300

    added test component 2

---
+++ src/components/TestComponent2.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TestComponent2 extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default TestComponent2;
